484ac208ad1beaa581345eec24c62e1625801370
EBIN JOHNY <ebinchemp@gmail.com>
2021-01-22 12:34:40 +0530

    Update stop cisco webex.py

--- stop cisco webex.py
+++ stop cisco webex.py
@@ -1,12 +1,24 @@
 import os
 import time
+import subprocess
+import time
+
+
 taskname ='"'
 taskname+='atmgr.exe'  #this is its name , the cisco pop up name
+#taskname+='NOTEPAD.exe'  #trouble shooting code
 taskname+='"'
-print(taskname +'will be terminated \n\n')
-try:
-    os.popen('taskkill /IM '+taskname+' /F')
-except:
-    print("task could not be terminated")
+while(True):
+    try:
+        #(os.popen('taskkill /IM '+taskname+' /F')) 
+        st = str(subprocess.check_output('taskkill /IM '+taskname+' /F',shell = True).decode('utf-8'))
+    except:
+        print("\nall instances terminated ; extiting program....")
+        time.sleep(2)
+        exit()
+        
+    else :
+
+        print(taskname +' terminated ')
     
-time.sleep(2)
+
